639ad44e6a38011c0ba8e0377c1605946377f6f4
Rick Wolchuk <rickwolchuk@gmail.com>
2014-08-14 11:27:16 -0500

    Initial Commit

---
+++ galaxyQuest/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  galaxyQuest
+//
+//  Created by Rick Wolchuk on 8/14/14.
+//  Copyright (c) 2014 Rick. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ galaxyQuest/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  galaxyQuest
+//
+//  Created by Rick Wolchuk on 8/14/14.
+//  Copyright (c) 2014 Rick. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
